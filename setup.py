d8500baaf4d24d29158f04a9ebc59647095d21fd
ManjeshKumarN <63561772+ManjeshKumarN@users.noreply.github.com>
2025-12-03 08:42:11 +0000

    Added the notebooks

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="src",
     version="0.0.1",
-    author="Manjesh,
+    author="Manjesh",
     author_email="manjesh2700c@gmail.com",
     packages=find_packages()
 )
